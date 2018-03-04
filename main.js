55f3b3a036f96da9fefdbec230639179fad60953
Alex Orga <alexorga.dev@gmail.com>
2018-03-03 18:27:09 -0600

    added variables for 1st challenge

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 console.log('hi');
+
+// add the sum of each players score
+// 
+var players = [
+    { 
+        name: "Bob", 
+        scores: [10, 65] 
+    }, 
+        { name: "Bill", 
+        scores: [90, 5] 
+    }, 
+        { name: "Charlie", 
+        scores: [40, 55] 
+    }
+]; // Displays "Bill"
+var players = [
+    { 
+        name: "Bob", 
+        scores: [15, 20] 
+    }, 
+    { 
+        name: "Bill", 
+        scores: [10, 15] 
+    }, 
+    { 
+        name: "Charlie", 
+        scores: [90, 10] 
+    }
+]; // Displays "Charlie"
+for(var i=0; i<players.length; i++){
+
+}
